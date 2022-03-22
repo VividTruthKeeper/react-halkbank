9a1c3941eb0bd9271af38e892358ca609bff657c
Aydogdy Agabayev <76523543+VividTruthKeeper@users.noreply.github.com>
2022-03-22 21:34:08 +0500

    inputs changed

--- src/components/CardStages/CardStage4.js
+++ src/components/CardStages/CardStage4.js
@@ -1,9 +1,6 @@
 // IMPORT MODULES
 import React, { useState, useRef, useEffect } from "react";
 
-// IMPORT COMPONENTS
-// import CustomSelect from "../global/CustomSelect";
-
 // IMPORT IMAGES
 import remove from "../../icons/remove.svg";
 import next from "../../icons/next.svg";
@@ -11,20 +8,13 @@ import arrow from "../../icons/arrow.svg";
 import next_reverse from "../../icons/next-reverse.svg";
 
 const CardStage4 = ({ setStage, data, setData }) => {
-  const [files, setFiles] = useState({
-    input1: undefined,
-    input2: undefined,
-    input3: undefined,
-  });
+  const [files, setFiles] = useState(data.file ? data.file : []);
 
-  const input1 = useRef();
-  const input2 = useRef();
-  const input3 = useRef();
   const [btnEnabled, setBtnEnabled] = useState(false);
   const [dropdown, setDropdown] = useState(false);
 
   useEffect(() => {
-    if (files.input1 || files.input2 || files.input3) {
+    if (files.length > 0) {
       setBtnEnabled(true);
     } else {
       setBtnEnabled(false);
@@ -62,90 +52,43 @@ const CardStage4 = ({ setStage, data, setData }) => {
           </div>
         </div>
         <div className="cd-bottom-4">
-          <div
-            className={
-              files.input1 ? (files.input1[0] ? "file filled" : "file") : "file"
-            }
-          >
+          <div>
+            <label htmlFor="file">Загрузить файл</label>
             <input
-              ref={input1}
+              id="file"
               type="file"
               onChange={(e) => {
-                setFiles({ ...files, input1: e.target.files });
+                setFiles([...files, e.target.files[0]]);
+                e.target.value = "";
               }}
             />
-            <p>
-              {files.input1
-                ? files.input1[0]
-                  ? files.input1[0].name
-                  : "Загрузить файл"
-                : "Загрузить файл"}
-            </p>
-            <div
-              className="remove"
-              onClick={() => {
-                setFiles({ ...files, input1: undefined });
-                input1.current.value = null;
-              }}
-            >
-              <img src={remove} alt="remove" />
-            </div>
-          </div>
-          <div
-            className={
-              files.input2 ? (files.input2[0] ? "file filled" : "file") : "file"
-            }
-          >
-            <input
-              ref={input2}
-              type="file"
-              onChange={(e) => {
-                setFiles({ ...files, input2: e.target.files });
-              }}
-            />
-            <p>
-              {files.input2
-                ? files.input2[0]
-                  ? files.input2[0].name
-                  : "Загрузить файл"
-                : "Загрузить файл"}
-            </p>
-            <div
-              className="remove"
-              onClick={() => {
-                setFiles({ ...files, input2: undefined });
-                input2.current.value = null;
-              }}
-            >
-              <img src={remove} alt="remove" />
-            </div>
-          </div>
-          <div className={files.input3 ? "file filled" : "file"}>
-            <input
-              ref={input3}
-              type="file"
-              onChange={(e) => {
-                setFiles({ ...files, input3: e.target.files });
-              }}
-            />
-            <p>
-              {files.input3
-                ? files.input3[0]
-                  ? files.input3[0].name
-                  : "Загрузить файл"
-                : "Загрузить файл"}
-            </p>
-            <div
-              className="remove"
-              onClick={() => {
-                setFiles({ ...files, input3: undefined });
-                input3.current.value = null;
-              }}
-            >
-              <img src={remove} alt="remove" />
-            </div>
           </div>
         </div>
+        <ul className="cd-4-files">
+          {files !== []
+            ? files.map((el, i) => {
+                if (el) {
+                  return (
+                    <li key={i}>
+                      <h4>{el.name}</h4>
+                      <div
+                        className="remove"
+                        onClick={() => {
+                          setFiles(
+                            files.filter((file) => {
+                              return file !== el;
+                            })
+                          );
+                        }}
+                      >
+                        <img src={remove} alt="remove" />
+                      </div>
+                    </li>
+                  );
+                }
+              })
+            : null}
+        </ul>
         <div className="cu-bottom card-stage-4-bottom">
           <button
             type="button"
@@ -172,47 +115,8 @@ const CardStage4 = ({ setStage, data, setData }) => {
             disabled={!btnEnabled}
             className="sign-btn cu-btn"
             onClick={() => {
+              setData({ ...data, file: files });
               setStage(5);
-              if (files.input1 && files.input2 && files.input3) {
-                setData({
-                  ...data,
-                  file: [files.input1[0], files.input2[0], files.input3[0]],
-                });
-              } else if (files.input1 && files.input2) {
-                setData({
-                  ...data,
-                  file: [files.input1[0], files.input2[0]],
-                });
-              } else if (files.input1 && files.input3) {
-                setData({
-                  ...data,
-                  file: [files.input1[0], files.input3[0]],
-                });
-              } else if (files.input2 && files.input3) {
-                setData({
-                  ...data,
-                  file: [files.input2[0], files.input3[0]],
-                });
-              } else {
-                if (files.input1) {
-                  setData({
-                    ...data,
-                    file: [files.input1[0]],
-                  });
-                }
-                if (files.input2) {
-                  setData({
-                    ...data,
-                    file: [files.input2[0]],
-                  });
-                }
-                if (files.input3) {
-                  setData({
-                    ...data,
-                    file: [files.input3[0]],
-                  });
-                }
-              }
             }}
           >
             <div>
